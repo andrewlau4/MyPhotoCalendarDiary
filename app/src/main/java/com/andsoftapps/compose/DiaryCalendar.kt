8c31d641790db259451249cf02bd4b02a43939e6
andrew lau <alau9998@hotmail.com>
2023-02-14 17:17:22 -0500

    commit

--- app/src/main/java/com/andsoftapps/compose/DiaryCalendar.kt
+++ app/src/main/java/com/andsoftapps/compose/DiaryCalendar.kt
@@ -1,5 +1,11 @@
 package com.andsoftapps.compose
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.tween
@@ -7,22 +13,29 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.with
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -35,17 +48,23 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import com.andsoftapps.navigation.Route
 import com.andsoftapps.utils.ValueHolder
 import com.andsoftapps.utils.YEAR_MONTH_FORMATED_STRING
@@ -58,6 +77,10 @@ import java.time.YearMonth
 
 private val SCREEN_SLIDE_ANIM_DURATION_MILLIS = 1000
 private val BOTTOM_SHEET_HEIGHT = 130.dp
+private val DAY_BOX_HEIGHT = 70.dp
+private val DAY_BOX_BODRDER = 0.1.dp
+
+private val TAG = "DiaryCalendarScreen"
 
 @Composable
 fun DiaryCalendarScreen(viewModel: DiaryCalendarViewModel = hiltViewModel()) {
@@ -235,10 +258,105 @@ fun LazyGridScope.DiaryCalendarContent(month: YearMonth, firstDayOfWeek: Int, to
     items(totalDaysInMonth,
         key = { index ->
             "${month.year}-${index}"
-        }) {
+        }) { index ->
+        val dayInMonth = if (index >= firstDayOfWeek) { index - firstDayOfWeek + 1 }
+                            else null
+
+        DayBox(dayInMonth, month)
 
     }
 }
+
+@Composable
+fun DayBox(dayInMonth: Int?, month: YearMonth) {
+
+    var expanded by remember { mutableStateOf(false) }
+    val dayImage = LocalDiaryEntities.current[dayInMonth]?.diaryCalendarEntity?.imagePath
+    val context = LocalContext.current
+    val saveImgCallBack = LocalSaveImgPath.current
+    var launchResultUri by remember { mutableStateOf<Uri?>( dayImage?.let { Uri.parse(it) }) }
+    val getContentLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            result?.let { nonNullableResult ->
+                when (nonNullableResult.resultCode == Activity.RESULT_OK) {
+                    true -> {
+                        val intent = nonNullableResult.data
+                        launchResultUri = intent?.data
+                        launchResultUri?.apply {
+                            context.getContentResolver()
+                                .takePersistableUriPermission(launchResultUri!!,
+                                    // flags
+                                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                                )
+
+                            saveImgCallBack(month, dayInMonth!!, launchResultUri)
+                        }
+                    }
+                    else -> {
+                        Log.d(TAG, "GetContent URI returns false")
+                    }
+                }
+            }
+        }
+
+    Card(
+        shape = RoundedCornerShape(3.dp),
+        modifier = Modifier.fillMaxWidth().height(DAY_BOX_HEIGHT)
+            .clickable {
+                if (dayInMonth != null) {
+                    expanded = true
+                }
+            },
+        border = BorderStroke(DAY_BOX_BODRDER, Color.Black)
+    ) {
+        Box {
+            if (dayInMonth != null && launchResultUri != null) {
+                AsyncImage(
+                    model = launchResultUri,
+                    contentDescription = "Image from photo picker",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(200.dp, 200.dp)
+                    //.clip(CircleShape)
+                )
+            }
+
+            if (dayInMonth != null) {
+                DayIcon(Modifier.align(Alignment.TopStart), dayInMonth.toString())
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun BoxScope.CalendarIconBackground(modifier: Modifier = Modifier,
+                                    brush: Brush,
+                                    content: @Composable BoxScope.() -> Unit) {
+    Box (modifier = modifier
+        .clip(CircleShape)
+        .background(
+            brush = brush),
+        content = content
+    )
+}
+
+
+@Composable
+fun BoxScope.DayIcon(modifier: Modifier = Modifier, text: String) {
+    CalendarIconBackground(modifier = modifier,
+        brush = Brush.horizontalGradient(listOf(
+            Color.Black,
+            Color.DarkGray))) {
+        Text("00", Modifier.align(Alignment.Center), color = Color.White.copy(alpha = 0f))
+        Text(text,
+            color = Color.White,
+            modifier = Modifier
+                .align(Alignment.Center))
+    }
+}
+
 
 @Composable
 fun DiaryCalendarBottomSheetNavigation(monthLambda: () -> YearMonth,
